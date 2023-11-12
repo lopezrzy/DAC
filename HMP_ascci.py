72aa3a2d298c297f9a58a0b071a239f578a8e2c2
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-11-13 00:40:33 +0300

    Update HMP_ascci.py

--- HMP_ascci.py
+++ HMP_ascci.py
@@ -11,7 +11,7 @@ THUM_34 = serial.Serial("/dev/ttyACM0",
                    xonxoff=False,
                    timeout=2)
 
-HUM_34 = io.TextIOWrapper(io.BufferedRWPair(THUM_34, THUM_34))
+HUM_34 = io.TextIOWrapper(io.BufferedRWPair(THUM_34, THUM_34), encoding='ascii')
 
 try:
     while True:
