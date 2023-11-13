19b440cb1aad28cbe7e1680c39894095aa410cf9
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-11-13 10:41:22 +0300

    Update temp1.py

--- temp1.py
+++ temp1.py
@@ -9,14 +9,14 @@ def initialize_sensor(port, sensor_number):
                                   parity=serial.PARITY_EVEN,
                                   stopbits=serial.STOPBITS_ONE,
                                   xonxoff=False,
-                                  timeout=2)
+                                  timeout=4)
     hum_sensor = io.TextIOWrapper(io.BufferedRWPair(serial_sensor, serial_sensor))
     return hum_sensor
 
 def get_readings(sensor_number, hum_sensor):
     try:
         while True:
-            hum_sensor.write(unicode(f"open {sensor_number}"))
+            hum_sensor.write(f"open {sensor_number}")
             print(f"Sensor {sensor_number}: open")
             hum_sensor.flush()
             sleep(1)
@@ -24,11 +24,9 @@ def get_readings(sensor_number, hum_sensor):
             print(f"Sensor {sensor_number}: send")
             hum_sensor.flush()
             sleep(1)
-            #data = hum_sensor.read(100)
-
             data = hum_sensor.readline()
-            dataInString=str(data)
-            print(f"Sensor {sensor_number}: data is {dataInString}")
+            #dataInString=str(data)
+            print(f"Sensor {sensor_number}: data is {data}")
             hum_sensor.flush()
             sleep(1)
             hum_sensor.write("close")
