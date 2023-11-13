67e8e4fc50d34e29d4d5c8e009144b701b881b97
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-11-13 12:07:30 +0300

    Update HMP-155-0_1.py

--- HMP-155-0_1.py
+++ HMP-155-0_1.py
@@ -44,7 +44,7 @@ def read_sensor(port, sensor_number):
         serial_sensor.close()
 
 # List of sensor numbers in the desired order
-sensor_numbers = [0, 1]
+sensor_numbers = [1,0]
 
 # Serial port for all sensors
 port = "/dev/ttyACM0"
