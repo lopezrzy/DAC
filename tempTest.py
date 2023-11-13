f38c418ed96db1fa8b01c188bfb9687c483e2bd2
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-11-13 11:41:50 +0300

    Update tempTest.py

--- tempTest.py
+++ tempTest.py
@@ -9,14 +9,14 @@ def initialize_sensor(port, sensor_number):
                                   parity=serial.PARITY_EVEN,
                                   stopbits=serial.STOPBITS_ONE,
                                   xonxoff=False,
-                                  timeout=2)
+                                  timeout=20)
     hum_sensor = io.TextIOWrapper(io.BufferedRWPair(serial_sensor, serial_sensor))
 
     try:
         while True:
             hum_sensor.write("R\r\n")  # Start continuous output
             hum_sensor.flush()
-            #print(f"Sensor {sensor_number}: started continuous output")
+            print(f"Sensor {sensor_number}: started continuous output")
             sleep(2)  # Allow time for the sensor to start continuous output
 
             hum_sensor.write("SEND\r\n")  # Request a reading
@@ -27,14 +27,22 @@ def initialize_sensor(port, sensor_number):
             # Optional: Stop continuous output if needed
             # hum_sensor.write("S\r\n")
             # hum_sensor.flush()
-
-            sleep(5)  # Adjust as needed based on your interval requirements
+            
+            # Add a KeyboardInterrupt handler to stop continuous output
+            try:
+                sleep(5)  # Adjust as needed based on your interval requirements
+            except KeyboardInterrupt:
+                hum_sensor.write("S\r\n")  # Stop continuous output
+                hum_sensor.flush()
+                print(f"Sensor {sensor_number}: stopped continuous output")
+                raise  # Re-raise KeyboardInterrupt to exit the loop
 
     except KeyboardInterrupt:
         # Clean up when interrupted
-        hum_sensor.write("S\r\n")
-        hum_sensor.flush()
         print(f"Sensor {sensor_number} Port Closed")
+    finally:
+        # Close the serial port to ensure clean termination
+        serial_sensor.close()
 
 # List of sensor numbers in the desired order
 sensor_numbers = [0]
