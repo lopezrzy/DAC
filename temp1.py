59b7160b5895ef8a6172bd3e0f0fb589fe068a02
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-11-13 10:04:07 +0300

    Update and rename HMP_z.py to temp1.py

---
+++ temp1.py
@@ -0,0 +1,46 @@
+import serial
+import io
+from time import sleep
+
+def initialize_sensor(port, sensor_number):
+    serial_sensor = serial.Serial(port,
+                                  baudrate=4800,
+                                  bytesize=serial.SEVENBITS,
+                                  parity=serial.PARITY_EVEN,
+                                  stopbits=serial.STOPBITS_ONE,
+                                  xonxoff=False,
+                                  timeout=2)
+
+    hum_sensor = io.TextIOWrapper(io.BufferedRWPair(serial_sensor, serial_sensor))
+
+    try:
+        while True:
+            hum_sensor.write(f"open {sensor_number}")
+            print(f"Sensor {sensor_number}: open")
+            hum_sensor.flush()
+            sleep(1)
+            hum_sensor.write("send")
+            print(f"Sensor {sensor_number}: send")
+            hum_sensor.flush()
+            sleep(1)
+            data = hum_sensor.readline()
+            print(f"Sensor {sensor_number}: data is {data}")
+            hum_sensor.flush()
+            sleep(1)
+            hum_sensor.write("close")
+            print(f"Sensor {sensor_number}: close")
+            sleep(2)
+
+    except KeyboardInterrupt:
+        # Clean up when interrupted
+        print(f"Sensor {sensor_number} Port Closed")
+
+# List of sensor numbers in the desired order
+sensor_numbers = [31, 32, 33, 34]
+
+# Serial port for all sensors
+port = "/dev/ttyACM0"
+
+# Loop through the sensors
+for sensor_number in sensor_numbers:
+    initialize_sensor(port, sensor_number)
