728e3fb7e876e48873813e63af33f0b237f2ab34
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-11-13 12:29:22 +0300

    Update HMP-155-0_1V1.py

--- HMP-155-0_1V1.py
+++ HMP-155-0_1V1.py
@@ -11,7 +11,7 @@ def read_sensor(port, sensor_number):
                                   parity=serial.PARITY_EVEN,
                                   stopbits=serial.STOPBITS_ONE,
                                   xonxoff=False,
-                                  timeout=20)
+                                  timeout=5)
     hum_sensor = io.TextIOWrapper(io.BufferedRWPair(serial_sensor, serial_sensor))
 
     try:
