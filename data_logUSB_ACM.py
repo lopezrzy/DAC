ee0b894e2ef1e2eca827c44d5b8d5aa869b0db21
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-12-13 19:45:15 +0300

    Update data_logUSB_ACM.py

--- data_logUSB_ACM.py
+++ data_logUSB_ACM.py
@@ -186,7 +186,7 @@ except KeyboardInterrupt:
             with open(data_pathway, mode='r') as original_csv_file:
                 csv_reader = csv.DictReader(original_csv_file)
                 for row in csv_reader:
-                    if 'Temp' in row and 'Humidity' in row:
+                    if 'Temp' in row and 'Humidity' in row and (row['Temp'] or row['Humidity']):
                         temp_humidity_writer.writerow({'Date': row['Date'], 'Time': row['Time'], 'IO': row['IO'], 'Temp': row['Temp'], 'Humidity': row['Humidity']})
 
         print(f"Temperature and humidity data saved to {temp_humidity_data_pathway}")
