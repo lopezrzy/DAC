ff76025494ee4f9fe47994f69ef7c28931ab7824
lopezrzy <140556110+lopezrzy@users.noreply.github.com>
2023-12-14 11:33:17 +0300

    Update minutelyAverage.py

--- minutelyAverage.py
+++ minutelyAverage.py
@@ -1,4 +1,36 @@
 import pandas as pd
+import yagmail
+import datetime
+
+
+def sendData(description, original_data, minutelyAv_data):
+    # Set up your yagmail instance
+    email_address = 'dacceakaust@gmail.com '
+    password = 'dac202312'
+    yag = yagmail.SMTP(email_address, password)
+    
+    email_address = 'bantanalamin@gmail.com'
+    password = 'muzomvmpwxiczzmo'
+    yag = yagmail.SMTP(email_address, password)
+    
+    try:
+      # Compose the email
+      rec = ['zulma.lopezreyes@kaust.edu.sa']
+      today_date = datetime.datetime.now().strftime('%Y-%m-%d')
+      today_date2 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+      subject_ = f'DAC data: {today_date}'      
+      body = f'{description} at {today_date2}' 
+      yag.send(to=rec, subject=subject_, contents=body, attachments= [original_data, minutelyAv_data])
+
+      print("Email sent successfully!")
+    
+    except Exception as e:
+      # Log the error
+      print(f"Error: {e}")
+    
+    finally:
+      # Logout
+      yag.close()
 
 # Function to calculate the minutely average
 def calculate_minutely_average(dataframe):
@@ -21,11 +53,16 @@ except FileNotFoundError:
     print("File not found. Please provide a valid file path.")
     exit(1)
 
+description = input("Description: ")
+
+
 # Calculate minutely averages
 minutely_avg_df = calculate_minutely_average(df)
 
 # Create a new CSV file with minutely averages
 output_path = input_path.replace('.csv', '_minutely.csv')
 minutely_avg_df.to_csv(output_path)
+
+sendData(description, input_path, output_path)
 
 print(f"Minutely averages saved to {output_path}")
